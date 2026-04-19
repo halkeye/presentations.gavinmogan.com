db50c8a831368ebf7f9432a7b9fdc7c942fd1c7c
Gavin Mogan <git@gavinmogan.com>
2026-04-19 12:10:08 -0700

    shared deck templates

---
+++ content/decks/decks.11tydata.js
@@ -0,0 +1,4 @@
+module.exports = {
+  tags: ["decks"],
+  layout: "layouts/deck.njk",
+};
--- content/presentations/presentations.11tydata.js
+++ content/presentations/presentations.11tydata.js
@@ -1,12 +1,10 @@
 module.exports = {
-	tags: [
-		"presentations"
-	],
-	"layout": "layouts/presentation.njk",
-	eleventyComputed: {
-		// data here is the post's data
-		permalink: function (data) {
-			return `/${data.slug ?? this.slugify(data.page.fileSlug)}/index.html`;
-		}
-	}
+  tags: ["presentations"],
+  layout: "layouts/presentation.njk",
+  eleventyComputed: {
+    // data here is the post's data
+    permalink: function (data) {
+      return `/${data.slug ?? this.slugify(data.page.fileSlug)}/index.html`;
+    },
+  },
 };
